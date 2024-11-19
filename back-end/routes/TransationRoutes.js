c92a607810ffac93a9b6dfe9963bc409f5e8e072
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-11-19 20:53:52 -0300

    Transation base created
    The transaction has been created, but the crypto input has been changed, not the output.
    
    So, if I buy some Bitcoin using Ethereum, the Bitcoin will be added to my wallet, but the Ethereum will not subtract from my wallet.
    
    furthermore, I do not make the conversion off the cryptos, and I do not work with moneys, I just cryptos

---
+++ back-end/routes/TransationRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import TransationController from "../controllers/TransationController.js";
+const TransationRoutes = express.Router();
+
+TransationRoutes.post("/buy", TransationController.purchaseCrypto);
+TransationRoutes.post("/sell", TransationController.sellCrypto);
+
+export default TransationRoutes;

00f74cce01eee14b850d0348bc87fc75f7d01e9d
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2024-11-24 22:32:36 -0300

    Transação de venda de cryptos criada
    A transação de venda de cryptos foi criada. Ela basicamente segue o modelo da de compra de cripto.

--- back-end/routes/TransationRoutes.js
+++ back-end/routes/TransationRoutes.js
@@ -4,5 +4,6 @@ import TransationController from "../controllers/TransationController.js";
 const TransationRoutes = express.Router();
 
 TransationRoutes.post("/buy", TransationController.purchaseCrypto);
+TransationRoutes.post("/sell", TransationController.sellCrypto);
 
 export default TransationRoutes;
